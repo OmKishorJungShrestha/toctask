f10c5a54c782db2398a699a55fce7f56a1d66f9e
Om Kishor Jung Shrestha <omshrestha40@gmail.com>
2024-12-28 17:50:36 +0545

    Update lab10nfaendswith01.c

--- lab10nfaendswith01.c
+++ lab10nfaendswith01.c
@@ -2,27 +2,26 @@
    A state in NFA may have multiple transitions for an input symbol. Such parallel
    computing can be better modeled with recursive functions. So, each transition from
    a state has been defined as a recursive function. */
+/* Implement NFA over alphabet {0,1} that accepts all strings ending with 01.
+   A state in NFA may have multiple transitions for an input symbol. Such parallel
+   computing can be better modeled with recursive functions. So, each transition from
+   a state has been defined as a recursive function. */
 #include<stdio.h>
 #include<string.h>
 
 char input[20]; // to store input string
-int l;          // to store length of the input string
-int flag;       // to decide accept or reject the input string
+int l; // to store length of the input string
+int flag = 0; // to decide accept or reject the input string
 
 void q2(int i) {
-    if (input[i] == '\0') {
+    if(input[i] == '\0') {
         flag = 1;
     }
 }
 
 void q1(int i) {
-    if (i < l) { // i is less than the length of the string
-        if (input[i] == '0') {
-            int k = i;
-            k++;
-            q0(k);
-            q1(k);
-        } else if (input[i] == '1') {
+    if(i < l) {
+        if(input[i] == '1') {
             i++;
             q2(i);
         }
@@ -30,11 +29,13 @@ void q1(int i) {
 }
 
 void q0(int i) {
-    if (i < l) {
-        if (input[i] == '0') {
-            i++;
-            q1(i);
-        } else if (input[i] == '1') {
+    if(i < l) { // if i is less than the length of the string
+        int k = i;
+        if(input[i] == '0') {
+            k++;
+            q0(k);
+            q1(k);
+        } else if(input[i] == '1') {
             i++;
             q0(i);
         }
@@ -42,16 +43,17 @@ void q0(int i) {
 }
 
 int main() {
-    printf("\nEnter a string: ");
+    printf("\nEnter a string: \t");
     gets(input);
     l = strlen(input);
     int i = 0;
     flag = 0;
     q0(i);
-    if (flag == 1)
+
+    if(flag == 1)
         printf("\nThe string is accepted.");
     else
         printf("\nThe string is not accepted.");
+
     return 0;
 }
-
